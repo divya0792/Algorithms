d56199ad73a23cce4bcfb913f099babbfc4097d2
Divya Sharma <divyasharma0792@gmail.com>
2018-09-12 23:47:40 -0700

    Update checkstyle

--- src/com/util/basic/sort/BubbleSort.java
+++ src/com/util/basic/sort/BubbleSort.java
@@ -7,10 +7,10 @@ package com.util.basic.sort;
  * time complexity = O(n^2)
  */
 public class BubbleSort {
-    public void sort(int[] arr){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length - 1 - i; j++){
-                if(arr[j] > arr[j + 1]){
+    public void sort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j + 1]) {
                     SortUtility.swap(arr, j, j + 1);
                 }
             }
--- src/com/util/basic/sort/SelectionSort.java
+++ src/com/util/basic/sort/SelectionSort.java
@@ -4,21 +4,21 @@ package com.util.basic.sort;
  * This class implements Selection Sort in ascending order
  * This method assumes first element as minIndex and keeps comparing with the
  * next one till minIndex has index of the lowest number and keeps sorting
- * space complexity = O(n)
+ * space complexity = O(1)
  * time complexity = O(n^2)
  */
 public class SelectionSort {
 
-    public void sort(int[] arr){
-        for(int i = 0;i < arr.length; i++){
+    public void sort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
             int minIndex = i;
-            for(int j = i + 1;j < arr.length; j++){
-                if(arr[j]< arr[minIndex]){
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-                if(minIndex != i){
-                    SortUtility.swap(arr,minIndex,i);
+                if(minIndex != i) {
+                    SortUtility.swap(arr, minIndex, i);
                 }
         }
     }
