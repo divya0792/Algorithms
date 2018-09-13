0f7fd8e23e32cdc2c3dc5881ea1a639df0a3ed73
Divya Sharma <divyasharma0792@gmail.com>
2018-09-12 23:39:34 -0700

    Implement Selection Sort

---
+++ src/com/util/basic/sort/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.util.basic.sort;
+
+/**
+ * This class implements Selection Sort in ascending order
+ * This method assumes first element as minIndex and keeps comparing with the
+ * next one till minIndex has index of the lowest number and keeps sorting
+ * space complexity = O(n)
+ * time complexity = O(n^2)
+ */
+public class SelectionSort {
+
+    public void sort(int[] arr){
+        for(int i = 0;i < arr.length; i++){
+            int minIndex = i;
+            for(int j = i + 1;j < arr.length; j++){
+                if(arr[j]< arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+                if(minIndex != i){
+                    SortUtility.swap(arr,minIndex,i);
+                }
+        }
+    }
+
+}
