41261b9c41fdfc746032f878e36be41f23af6207
Divya Sharma <Divya@Divyas-MacBook-Pro.local>
2018-11-06 00:04:04 -0800

    Leetcode_811 Subdomain visit count

--- src/com/leetcode/easy/Leetcode_811.java
+++ src/com/leetcode/easy/Leetcode_811.java
@@ -1,4 +1,52 @@
 package com.leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Leetcode_811 {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> eachVisit = new ArrayList<>();
+
+        for (String cpdomain : cpdomains) {
+            String[] split = cpdomain.split(" ");
+            String[] domain = split[1].split("\\.");
+            fillMap(domain, Integer.valueOf(split[0]), map);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            eachVisit.add(entry.getValue() + " " + entry.getKey());
+        }
+
+        return eachVisit;
+
+    }
+
+    private void fillMap(String[] domain, int count, Map<String, Integer> map) {
+        String currDomain = "";
+
+        for(int i = domain.length - 1; i >= 0; i--) {
+
+            if(currDomain.length() == 0) {
+                currDomain = domain[i];
+            } else {
+                currDomain = domain[i] + "." + currDomain;
+            }
+
+            if(map.containsKey(currDomain)) {
+                map.put(currDomain, map.get(currDomain) + count);
+            } else {
+                map.put(currDomain, count);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Leetcode_811 leet = new Leetcode_811();
+
+        String[] cpDomains = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
+        leet.subdomainVisits(cpDomains);
+    }
 }
--- src/com/leetcode/easy/Leetcode_852.java
+++ src/com/leetcode/easy/Leetcode_852.java
@@ -1,4 +1,34 @@
 package com.leetcode.easy;
 
 public class Leetcode_852 {
+
+    public int peakIndexInMountainArray(int[] A) {
+        if(A == null || A.length < 3) {
+            return -1;
+        }
+        return findPeak(A, 0, A.length - 1);
+
+
+    }
+    private int findPeak(int[] A, int low, int high) {
+        if(low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+
+        if(A[mid - 1] < mid && A[mid + 1] < A[mid]) {
+            return mid;
+        } else if(A[mid - 1] > A[mid]) {
+            return findPeak(A, low, mid - 1);
+        } else {
+            return findPeak(A, mid + 1, high);
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Leetcode_852 leet = new Leetcode_852();
+        int[] A = {3,4,5,1};
+        leet.peakIndexInMountainArray(A);
+    }
 }
